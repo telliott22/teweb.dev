3bebe558150c11f261a3c16cd36294df5f85975b
Tim Elliott <telliott22@gmail.com>
2021-10-13 17:23:02 +0200

    :construction: favicon

---
+++ components/blocks/tech.js
@@ -0,0 +1,17 @@
+import Container from '../container'
+
+export default function Projects({}){
+
+    return (
+        <section className="min-h-screen bg-white text-black">
+
+            <Container>
+                <ul>
+                    <li>React</li>
+                </ul>
+            </Container>
+
+        </section>
+    )
+
+}
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,29 @@
 import Layout from '../components/layout'
+import Head from 'next/head'
 import Hero from '../components/blocks/hero'
 import About from '../components/blocks/about'
 import Projects from '../components/blocks/projects'
+import Tech from '../components/blocks/tech'
 import CustomCursor from '../components/custom-cursor.js'
 
 function renderPage({ pageData, settings }) {
   return (
       <Layout > 
 
+        <Head>  
+          <title>Tim Elliott - Web Developer based in Berlin</title>
+          <link rel="icon" type="image/svg+xml" href="favicon.svg" />
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <meta name="description" content="Tim Elliott is a web developer based in Berlin. He is passionate about building websites and web applications." />
+        </Head>
+
         <Hero />
 
         <Projects />
 
         <About />
+
+        <Tech />
 
         <CustomCursor />
 
